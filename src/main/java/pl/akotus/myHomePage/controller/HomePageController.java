4fcdd592da6fc78ce4438b119d3ec38c9cd4d12d
a.kotus <a.kotus@gmail.com>
2019-12-05 19:39:30 +0100

    first commit

---
+++ src/main/java/pl/akotus/myHomePage/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package pl.akotus.myHomePage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomePageController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView homePageGet() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+}
---
+++ src/test/java/pl/akotus/myHomePage/MyHomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akotus.myHomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
